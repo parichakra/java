3c3d62168027407783ec1558a36d0521220e7417
parichakra <khatrivikram005@gmail.com>
2025-01-26 12:31:30 -0500

    for each loop

---
+++ one /ForEachLoop.java
@@ -0,0 +1,9 @@
+public class ForEachLoop{
+    public static void main (String [] args ){
+        String [] fruits = {"apple ", "banana ", "pear", "peach"};
+        for (String i : fruits){
+            System.out.println(i);
+        }
+    }
+
+}
