cac3d51e54f81860ab4d8316df35ef5db91d505f
parichakra <khatrivikram005@gmail.com>
2025-01-27 21:02:15 -0500

    interface added

--- one /ForEachLoop.java
+++
@@ -1,9 +0,0 @@
-public class ForEachLoop{
-    public static void main (String [] args ){
-        String [] fruits = {"apple ", "banana ", "pear", "peach"};
-        for (String i : fruits){
-            System.out.println(i);
-        }
-    }
-
-}
