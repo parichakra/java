9e930fec94793884b3df025ea82ff56eb6d2dee9
parichakra <khatrivikram005@gmail.com>
2025-01-29 14:21:56 -0500

    custome package added

---
+++ oops /CustomePackage/ImpClass.java
@@ -0,0 +1,12 @@
+import mypack.MyClass;
+
+class ImpClass{
+    public static void main ( String[] args){
+        //A object is made ow of MyClass
+
+        MyClass obj = new MyClass();
+
+        obj.msg();
+    }
+}
+
