dce78768423f082f0ebaf54d1f624385d7eade8d
parichakra <khatrivikram005@gmail.com>
2025-01-29 20:07:58 -0500

    Constructor in Java

---
+++ oops /Constructor/DefaultConstructor.java
@@ -0,0 +1,11 @@
+class Student{
+    Student(){
+        System.out.println("Default constructor called ");
+    }
+}
+
+public class DefaultConstructor{
+    public static void main( String[] args){
+        Student s1= new Student();
+    }
+}
